5d2fed83de160881137409f1ba3a87aa87fd1a6d
Aerttyz <alesandrosilva2004@gmail.com>
2025-03-26 13:31:51 -0300

    feat: function to download PDFs

--- TESTES-DE-NIVELAMENTO/scraping/pdf_downloader.py
+++ TESTES-DE-NIVELAMENTO/scraping/pdf_downloader.py
@@ -1,7 +1,23 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import os
 
+
+pdf_path = "/content/pdfs"
+os.makedirs(pdf_path, exist_ok=True)
+
+def download_pdf(pdf_url, pdf_name):
+  try:
+    response = requests.get(pdf_url)
+    response.raise_for_status()
+    pdf_file = os.path.join(pdf_path, pdf_name+".pdf")
+    with open(pdf_file, 'wb') as file:
+      file.write(response.content)
+    print(f"pdf salvo em: {pdf_file}")
+  except requests.exceptions.RequestException as e:
+    print(f"Erro ao baixar o PDF: {e}")
+    return None
 
 
 url =  "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
@@ -9,7 +25,6 @@ url =  "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-socieda
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
-
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
