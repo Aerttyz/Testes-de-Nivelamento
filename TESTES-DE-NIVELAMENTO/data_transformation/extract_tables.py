7b677a3f51dc0fbec90b0e93575e12e537f437b8
Aerttyz <alesandrosilva2004@gmail.com>
2025-03-27 00:06:51 -0300

    feat: compress extracted CSV file after table extraction

--- TESTES-DE-NIVELAMENTO/data_transformation/extract_tables.py
+++ TESTES-DE-NIVELAMENTO/data_transformation/extract_tables.py
@@ -1,11 +1,15 @@
 import pdfplumber
 import pandas as pd
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from scraping import compressor
+
 
 pdf_path = "/content/pdfs/Anexo I..pdf"
 
 with pdfplumber.open(pdf_path) as file:
   tables = []
-  print(len(file.pages))
   for number_of_pages in file.pages:
     table = number_of_pages.extract_table()
     if table:
@@ -15,4 +19,9 @@ df = pd.DataFrame(tables[1:], columns=tables[0])
 
 df.rename(columns=lambda x: x.replace("OD", "Seg. Odontológica").replace("AMB", "Seg. Ambulatorial"), inplace=True) 
 
-df.to_csv("saida.csv", index=False)
+saida_csv_path = "/content/csv/saida.csv"
+os.makedirs("/content/csv", exist_ok=True)
+df.to_csv(saida_csv_path, index=False)
+
+os.makedirs("/content/compact_files", exist_ok=True)
+compressor.compact_files(saida_csv_path, "/content/compact_files/compact_csv.zip")
