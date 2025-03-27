b47210d19fdc4f842e7528f8da5e6a13e506027d
Aerttyz <alesandrosilva2004@gmail.com>
2025-03-27 00:06:11 -0300

    feat: add directory check in file compression function

--- TESTES-DE-NIVELAMENTO/scraping/compressor.py
+++ TESTES-DE-NIVELAMENTO/scraping/compressor.py
@@ -4,9 +4,12 @@ import os
 
 def compact_files(input_path, compact_file_path):
     with ZipFile(compact_file_path, 'w') as zip:
-        for file_name in os.listdir(input_path):
-            if file_name.endswith('.pdf'):
-                file_path = os.path.join(input_path, file_name)
-                zip.write(file_path, arcname=file_name)
+        if os.path.isdir(input_path):
+            for file_name in os.listdir(input_path):
+                if file_name.endswith('.pdf'):
+                    file_path = os.path.join(input_path, file_name)
+                    zip.write(file_path, arcname=file_name)
+        else:
+            zip.write(input_path, arcname=os.path.basename(input_path))
     print(f"Arquivos compactados em: {compact_file_path}")
 
