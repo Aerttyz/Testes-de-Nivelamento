244d913759a641a76b3f6e812cd7b3f2a530cf13
Aerttyz <alesandrosilva2004@gmail.com>
2025-03-30 17:25:12 -0300

    fix: replace '=' with 'LIKE' in SQL query for pattern matching

--- TESTES-DE-NIVELAMENTO/bd_teste/analytic_query_last_quarter.sql
+++ TESTES-DE-NIVELAMENTO/bd_teste/analytic_query_last_quarter.sql
@@ -2,6 +2,6 @@ SELECT *,
        (vl_saldo_final * 100) - (vl_saldo_inicial * 100) AS diff 
 FROM teste.demonstracoes_contabeis 
 WHERE data = '2024-10-01' 
-AND descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ' 
+AND descricao LIKE 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR ' 
 ORDER BY diff ASC
 LIMIT 10;
